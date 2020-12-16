7974a17286e29ad85ebcbf8c52189209d252de96
Snail <jyc356@gmail.com>
2020-12-16 21:15:42 +0800

    登入頁面製作+登出

--- login.php
+++ login.php
@@ -1,53 +1,69 @@
 <?php
-$title = '登入'
+if(! isset($_SESSION)){
+    session_start();
+}
+$title = '登入';
 
-
-if(isset($_POST['account']) and $_POST['Password'])){
-    if($_POST['account']==='test' and $_POST['Password']==='123'){
-        //可以登入
-    }else{
-        $errorMsg = '帳號或密碼輸入錯誤'
+if(isset($_POST['account']) and isset($_POST['password']) ){
+    if($_POST['account']==='test' and $_POST['password']==='123'){
+        // 可以登入
+        $_SESSION['admin'] = 'test';
+    } else {
+        $errorMsg = '帳號或密碼錯誤';
     }
 }
 ?>
-
-<?php include __DIR__ . '/parts/html-head.php' ?>
-<?php include __DIR__ . '/parts/navbar.php' ?>
-
+<?php include __DIR__. '/parts/html-head.php'; ?>
+<?php include __DIR__. '/parts/navbar.php'; ?>
 <div class="container">
     <div class="row d-flex justify-content-center">
         <div class="col-lg-6">
-            <div class="card-body">
-                <h5 class="card-title">登入</h5>
-                <!-- 表單刷新送出 -->
-                <form method="post">
-                    <div class="form-group">
-                        <label for="account">account</label>
-                                                         <!-- 一定要加name才有效可以送資料 -->
-                        <input type="text" class="form-control" id="account" name="account"
-                        value="<?= htmlentities($_POST['account'] ?? '' )?>">
-                        
-                    </div>
-                    <div class="form-group">
-                        <label for="Password">Password</label>
-                        <input type="password" class="form-control" id="Password" name="Password" 
-                        value="<?= htmlentities($_POST['account'] ?? '' )?>">
-                    </div>
-                    <div class="form-group form-check">
-                    <!-- checkbox預設送出on也可自己設定value checked可以讓勾選停留不會送出就消失-->
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1" name="exampleCheck1"
-                        value="123" checked
-                        echo isset($_POST['exampleCheck1']) ? 'checked' : '' >
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+            <?php if(isset($errorMsg)): ?>
+            <div class="alert alert-danger" role="alert">
+                <?= $errorMsg ?>
             </div>
+            <?php endif ?>
+            <?php if(isset($_SESSION['admin'])): ?>
+            <div>
+                <h3>Hello <?= $_SESSION['admin'] ?></h3>
+                <p><a href="logout.php">登出</a></p>
+            </div>
+            <?php else: ?>
+            <div class="card" >
+                <div class="card-body">
+                    <h5 class="card-title">登入</h5>
 
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="account">Account</label>
+                            <input type="text" class="form-control" id="account" name="account"
+                                    value="<?= htmlentities($_POST['account']  ?? '') ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" name="password"
+                                   value="<?= htmlentities($_POST['password'] ?? '')  ?>">
+                        </div>
+                        <!--
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" value="1"
+                                   id="exampleCheck1" name="exampleCheck1"
+                                   <?php // echo isset($_POST['exampleCheck1']) ? 'checked' : '' ?> >
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        -->
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+
+                </div>
+            </div>
+            <?php endif ?>
         </div>
     </div>
+
+
+
 </div>
-
-
-<?php include __DIR__ . '/parts/scripts.php' ?>
-<?php include __DIR__ . '/parts/html-foot.php' ?>
+<?php include __DIR__. '/parts/scripts.php'; ?>
+<?php include __DIR__. '/parts/html-foot.php'; ?>
