70f42fb5f7765b10e982f6c448b8bb33f08e595a
Snail <jyc356@gmail.com>
2020-12-20 14:44:43 +0800

    顯示前選取頁面

--- ab_list.php
+++ ab_list.php
@@ -1,7 +1,8 @@
 <?php
 require __DIR__ . '/db_connect.php';
 $pageName = 'ab-list';
-
+// 由使用者給數值 如果有的話轉為整數，沒的話給1
+$page = isset($_GET['page']) ? intval($_GET['page']):1;
 
 $perPage = 5;
 $t_sql = "SELECT COUNT(1) FROM snail_class";
@@ -10,9 +11,16 @@ $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
 // $totalRows = $pdo->query($t_sql)->fetch()['COUNT(1)'];
 // 總共頁數
 $totalPages = ceil($totalRows/$perPage);
+// 限定page的範圍
+if($page<1) $page=1;
+if($page>$totalPages) $page = $totalPages;
 
+// SELECT * FROM `snail_class` LIMIT 4,3  LIMIT(索引值,筆數)
+$p_sql = sprintf("SELECT * FROM snail_class LIMIT %s, %s" ,
+($page-1)*$perPage, $perPage);
 
-$stmt = $pdo->query("SELECT * FROM snail_class ORDER BY sid DESC");
+$stmt = $pdo ->query($p_sql);
+// $stmt = $pdo->query("SELECT * FROM snail_class ORDER BY sid DESC");
 
 $row = $stmt->fetchAll();
 
@@ -42,7 +50,9 @@ $row = $stmt->fetchAll();
 
                     <!-- 計算頁數的迴圈 -->
                     <?php for($i = 1 ; $i < $totalPages; $i++ ):?>
-                    <li class="page-item"><a class="page-link" href="#">
+                    <!-- ?是在同一頁面有不同的值 -->
+                    <li class="page-item <?= $page == $i ? 'active' : '' ?>" >
+                    <a class="page-link" href="?page=<?= $i ?>">
                         <?= $i ?></a>
                     </li>
                      <?php endfor ?>
