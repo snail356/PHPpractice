f1a41c8c271264f6ab6925ba57986be032b7e5f9
Snail <jyc356@gmail.com>
2020-12-20 23:17:49 +0800

    錯誤訊息跳出

--- ab-insert.php
+++ ab-insert.php
@@ -9,7 +9,10 @@ $pageName = 'ab-insert';
 <?php include __DIR__. '/parts/html-head.php'; ?>
 
 <style>
-    
+.form-text{
+    color: red;
+}
+
 </style>
 
 <?php include __DIR__. '/parts/navbar.php'; ?>
@@ -27,10 +30,11 @@ $pageName = 'ab-insert';
                 <div class="card-body">
                     <h5 class="card-title">新增課程</h5>
 
-                    <form method="post" novalidate onsubmit="checkFrom();return false;">
+                    <form method="post" novalidate onsubmit="checkForm();return false;">
                         <div class="form-group">
                             <label for="category">課程分類</label>
                             <input type="text" class="form-control" id="category" name="category">
+                            <small class="form-text error-msg" style="display: none">請選擇課程分類</small>
                         </div>
                         <div class="form-group">
                             <label for="classname">課程名稱</label>
@@ -60,6 +64,18 @@ $pageName = 'ab-insert';
 <?php include __DIR__. '/parts/scripts.php'; ?>
 
 <script>
+// document.querySelector('#category').style.bordercolor = 'red'
+const  category = document.querySelector('#category');
+
+function checkForm(){
+    if(category.value.length < 2){
+        category.style.borderColor = 'red';
+        let  small = category.closest('.form-group').querySelector('small');
+        small.innerText = "請輸入正確的名字";
+        small.style.display = 'block';
+    }
+
+}
 
 
 </script>
